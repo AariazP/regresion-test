6dfc1f14c3e417964abdc0d53f5ea89a699ec548
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2024-05-03 15:34:11 -0500

    (UPDATE) Test comment 2

--- src/main/java/evovital/uniquindio/edu/co/Main.java
+++ src/main/java/evovital/uniquindio/edu/co/Main.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     //add comment
-    //comment
+    //comment 2
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
